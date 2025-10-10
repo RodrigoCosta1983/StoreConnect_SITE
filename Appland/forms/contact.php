39ad603e6d5d8821c64bb364892752e503a6729d
RodrigoCosta1983 <rodrigo25.inf@outlook.com>
2025-10-10 15:47:37 -0300

    Updt e-mail

--- Appland/forms/contact.php
+++ Appland/forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'contato.rodrigocosta.dev@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,18 +24,18 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'contato.rodrigocosta.dev@gmail.com',
+    'username' => 'contato.rodrigocosta.dev@gmail.com',
+    'password' => 'Sophie#1983',
     'port' => '587'
   );
-  */
+  
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'Para');
+  $contact->add_message( $_POST['email'], 'E-mail');
+  $contact->add_message( $_POST['message'], 'Mensagem', 10);
 
   echo $contact->send();
 ?>
--- Appland/forms/newsletter.php
+++ Appland/forms/newsletter.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'contato.rodrigocosta.dev@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject ="New Subscription: " . $_POST['email'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'contato.rodrigocosta.dev@gmail.com',
+    'username' => 'contato.rodrigocosta.dev@gmail.com',
+    'password' => 'Sophie#1983',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['email'], 'Email');
 
